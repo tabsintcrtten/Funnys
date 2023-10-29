d4a6a71b912de76f98611663feb83b139ba10c93
Tab•Crtten <6w3-lblrmcv35@dingtalk.com>
2023-10-29 15:12:23 +0800

    Update FunnyCodesScript.js

--- FunnyCodes/FunnyCodesScript.js
+++ FunnyCodes/FunnyCodesScript.js
@@ -1,4 +1,13 @@
 window.onload = function(){
     alert('Page onload!');
-    
+    alert('Setting...');
+    var code = 0;
+    var CodeSection = document.getElementById('codes');
+    if(String['fromCharCode']){
+        alert('Setting end!');
+    }else{
+        alert('String.fromCharCode() can not use!');
+        window.close();
+    }
+    alert('Starting...')
 }
